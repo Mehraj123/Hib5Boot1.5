0e7c78d4d6b510c05e517f6d17876e469a0087da
Mehrajuddin Malik1 <mehrajuddin.malik1@synapse.com>
2018-04-06 13:18:53 +0530

    Initial commit

---
+++ src/main/java/com/hib/Application.java
@@ -0,0 +1,42 @@
+package com.hib;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import com.hib.entity.Person;
+
+@SpringBootApplication
+public class Application implements CommandLineRunner {
+
+	private SessionFactory sessionFactory;
+
+	public Application(SessionFactory sessionFactory) {
+		this.sessionFactory = sessionFactory;
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Session session = sessionFactory.openSession();
+		session.beginTransaction();
+
+		/*
+		 * // Check database version String sql = "select version()";
+		 * 
+		 * String result = (String) session.createNativeQuery(sql).getSingleResult();
+		 * System.out.println("SQL version is : "+result);
+		 */
+
+		Person person = new Person("Stark Jr.");
+		session.save(person);
+		System.out.println("Saved!!");
+		session.getTransaction().commit();
+		session.close();
+	}
+}
---
+++ src/main/java/com/hib/config/HinernateConfig.java
@@ -0,0 +1,40 @@
+package com.hib.config;
+
+import javax.annotation.PostConstruct;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.Metadata;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class HinernateConfig {
+
+	private StandardServiceRegistry standardServiceRegistry;
+	private SessionFactory sessionFactory;
+
+	@Bean
+	public SessionFactory sessionFactory() {
+		try {
+			standardServiceRegistry = new StandardServiceRegistryBuilder().configure().build();
+			Metadata metadata = new MetadataSources(standardServiceRegistry).getMetadataBuilder().build();
+			sessionFactory =  metadata.getSessionFactoryBuilder().build();
+			return sessionFactory;
+		} catch (Exception e) {
+			e.printStackTrace();
+			if (standardServiceRegistry != null) {
+				StandardServiceRegistryBuilder.destroy(standardServiceRegistry);
+			}
+		}
+		return sessionFactory;
+	}
+	
+	@PostConstruct
+	public void post() {
+		System.out.println("SessionFactory Created");
+	}
+
+}
